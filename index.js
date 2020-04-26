d2929e0eafc579e1557091a00732699ba7f05a40
Pyry <vuorelapyry@gmail.com>
2020-04-26 16:28:44 +0300

    sec commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const moment = require('moment');
 
 let connection = mysql.createConnection({
   host     : process.env.MYSQL_HOST || 'localhost',
-  user     : process.env.MYSQL_USER || 'root',
-  password : process.env.MYSQL_PASSWORD || 'root',
+  user     : process.env.MYSQL_USER || 'backend',
+  password : process.env.MYSQL_PASSWORD || 'backend',
 });
 
 let valve01 = 0;
